ff72e92e173e48f067dae886da8c876af025a580
procs <procs@main.lan>
2017-10-06 01:23:00 +0200

    almost dynamic records

--- myrecord.erl
+++ myrecord.erl
@@ -2,11 +2,15 @@
 %-export([test/0]).
 -compile(export_all).
 
--record(name,{field1 = "bla",flied2 , field3}).
-
+-record(zname,
+        {
+        field1 = "bla",
+        flied2, 
+        field3
+        }).
 stuff(Value1, Value2) ->
     %#name{field1=Value1},
-    #name{field1=Value2}.
+    #zname{field1=Value2}.
 
 %changevalue(Field, Value)  when is_atom(Field) ->
 %changevalue(Field=#name.field1, Value) ->
@@ -14,6 +18,31 @@ stuff(Value1, Value2) ->
 
 %queryFieldname(Fieldname) ->
 queryFieldname(Fieldname) ->
-    P = #name{field1 = "asdasdasd",field3="ddd"},
+    P = #zname{field1 = "asdasdasd",field3="ddd"},
     Name = element(4,P).
 
+record_fields() ->
+    %record_info(size, name).
+    record_info(fields, name).
+
+setFieldsToVariables() ->
+    List = record_fields().
+
+recurseList([H|D],List) ->
+    Field = H,
+    recurseList(D,[Field|List]);
+recurseList([],List) ->
+    List.
+
+this_element() ->
+    P = #zname{field1 = "asdasdasd",field3="ddd"},
+    P2 = setelement(#zname.field1,P,"setted").
+
+recurseElements([H|D],List) ->
+    Testname = #zname{field1 = "testfield1",field3="testfield3"},
+    Scopename = setelement(1,Testname,"setted"),
+    io:format("rec ~p~n",     [Scopename]),
+    Field = H,
+    recurseElements(D,[Field|List]);
+recurseElements([],List) ->
+    List.
---
+++ name.hrl
@@ -0,0 +1,7 @@
+-module(name).
+-record(name,
+        {
+        field1 = "bla",
+        flied2, 
+        field3
+        }).
