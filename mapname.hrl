250e7ae183eaf5f06741275da29c22669701a663
procs <procs@net.lan>
2017-10-06 23:11:27 +0200

    mapping - header files

---
+++ mapname.hrl
@@ -0,0 +1,12 @@
+
+
+testfunction() ->
+    Map = #{"hi" => 42},
+    MyMap = #{
+        "Field1" => 123,
+        "Field2" => 456,
+        "Field3" => "asd"
+        },
+    {Map, MyMap}.
+
+%-record(message_to,{to_name, message}).
--- mymap.erl
+++ mymap.erl
@@ -1,10 +1,13 @@
 -module(mymap).
 %-export([test/0]).
 -compile(export_all).
-
+-include("./mapname.hrl").
 %test() ->
 % ok.
 mapfunction() ->
     M1 = #{"Key" => value},
     Variable = "Key",
     maps:get(Variable, M1).
+
+mymapfunction(Variable) ->
+    Variable.
